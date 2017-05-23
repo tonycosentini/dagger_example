f6b96f3c795de569b5906f382ccc733aaf991bbc
Tony Cosentini <tony@endlesswhileloop.com>
2017-05-22 20:45:06 -0700

    Add a main method.

---
+++ src/main/java/dagger_example/Main.java
@@ -0,0 +1,11 @@
+package dagger_example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ParentComponent parentComponent = DaggerParentComponent.builder().build();
+
+        parentComponent.integer();
+        parentComponent.string();
+    }
+}
